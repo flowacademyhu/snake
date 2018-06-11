57d8683754a109ee15f47a081570c42318a7d097
dudaseva <dudas.eva.02@gmail.com>
2018-06-11 16:09:08 +0200

    map

---
+++ map.js
@@ -0,0 +1,18 @@
+const { table } = require('table');
+
+let size = 15;
+let map = [];
+
+function generateMap() {
+
+  for (let i = 0; i < size; i++) {
+    map[i] = [];
+    for (let j = 0; j < size; j++) {
+      map[i][j] = '';
+    }
+  }
+  console.log(map);
+}
+generateMap();
+
+
