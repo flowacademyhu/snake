a40bd2e8bddc4c1e16a222d64a129bc6ff3db880
Gabor Thury <thury.gabor1994@gmail.com>
2018-06-12 16:52:06 +0200

    modified to arrow function

--- generateFood.js
+++ generateFood.js
@@ -1,7 +1,7 @@
-function generateFood (map) {
-  let size = map.length;
+const generateFood = (map) => {
+  let size = map[i].length;
   let i = Math.floor(Math.random() * size);
   let j = Math.floor(Math.random() * size);
   map[i][j] = 'x';
-}
+};
 module.exports = generateFood;
