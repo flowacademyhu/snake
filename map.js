c4e4f3305fca89dab8a1cfecf9a0c3c0dc7049b1
GaborThury <39524334+GaborThury@users.noreply.github.com>
2018-06-12 10:17:05 +0200

    Add files via upload

---
+++ map.js
@@ -0,0 +1,20 @@
+//MAP
+const { table } = require('table');
+
+let size = 15;
+let map = [];
+
+function generateMap() {
+
+  for (let i = 0; i < size; i++) {
+    map[i] = [];
+    for (let j = 0; j < size; j++) {
+      map[i][j] = '';
+    }
+  }
+  console.log(map);
+}
+
+generateMap();
+
+module.exports = generateMap;
