dfe8af8a9d40ff4a1fb434640185404e8b00b77f
GaborThury <39524334+GaborThury@users.noreply.github.com>
2018-06-12 14:48:47 +0200

    Revert "T6 generate food"

--- generateFood.js
+++
@@ -1,7 +0,0 @@
-function generateFood (map) {
-  let size = map.length;
-  let i = Math.floor(Math.random() * size);
-  let j = Math.floor(Math.random() * size);
-  map[i][j] = 'x';
-}
-module.exports = generateFood;
