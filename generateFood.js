427cf1ad3352e4f1af6b36c5485016c404700ffb
Gabor Thury <thury.gabor1994@gmail.com>
2018-06-15 09:48:44 +0200

    added food generate at startup

--- generateFood.js
+++ generateFood.js
@@ -1,7 +1,7 @@
 const generateFood = (map) => {
-  let size = map[0].length;
+  let size = map.length;
   let i = Math.floor(Math.random() * size);
   let j = Math.floor(Math.random() * size);
-  map[i][j] = 'x';
+  map[i][j] = 'o';
 };
 module.exports = generateFood;
