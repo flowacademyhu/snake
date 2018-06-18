915a991fc86ba5f0eacd51afc0897d1322a5d0ee
kosoczky <kosoczky.adam@gmail.com>
2018-06-18 20:39:21 +0200

    final move

--- mapSnakeImport.js
+++ mapSnakeImport.js
@@ -2,42 +2,22 @@ const { table } = require('table');
 const readlineSync = require('readline-sync');
 const clear = require('terminal-clear');
 const snake = require('./snake');
+const terminalKit = require('terminal-kit').terminal;
 let position = snake.defaultPosition;
-//const generateFood = require('./generateFood');
+// const generateFood = require('./generateFood');
 const food = require('./generateFood');
 // map behívás
 const basicMap = require('./map');
+let direction = 'd';
 
+terminalKit.grabInput();
+terminalKit.on('key', function (key) {
+  if (key === 'q') { process.exit(); }
+  direction = key;
+  console.log(key);
+});
 
-// MOVE
-
-let currentMap = basicMap;
-const mapReset = () => {
-  for (let i = 0; i < currentMap.length; i++) {
-    for (let j = 0; j < currentMap[i].length; j++) {
-      currentMap[i][j] = ' ';
-    }
-  }
-};
-let counter = 0;
-while (true) { /* végtelenített while ciklus */
-  counter++;
-  clear();
-  mapReset();
-  console.log(position[0].x, position[0].y);
-  console.log(position[1].x, position[1].y);
-  console.log(position[2].x, position[2].y);
-  for (let positionIndex in position) {
-    let coordinate = position[positionIndex];
-    currentMap[coordinate.y][coordinate.x] = 'X';
-  }
-  food(counter, currentMap);
-  let modifiedMap = table(currentMap);
-  console.log(modifiedMap);
-  let index = readlineSync.keyIn('', { limit: 'wasdq' });
-
-  module.exports = modifiedMap;
-  // lépések
+const movement = (index) => {
   switch (index) {
     case 'w':
       snake.up(position);
@@ -52,8 +32,40 @@ while (true) { /* végtelenített while ciklus */
       snake.right(position);
       break;
   }
-  if (index === 'q' || position[0].y < 0 || position[0].x < 0 || position[0].y >= currentMap.length || position[0].x >= currentMap[0].length) {
-    console.log('Game Over!')
-    break;
+};
+
+// MOVE
+
+let currentMap = basicMap;
+const mapReset = () => {
+  for (let i = 0; i < currentMap.length; i++) {
+    for (let j = 0; j < currentMap[i].length; j++) {
+      currentMap[i][j] = ' ';
+    }
   }
-}
+};
+let counter = 0;
+const main = () => {
+  counter++;
+  clear();
+  mapReset();
+  console.log(position[0].x, position[0].y);
+  console.log(position[1].x, position[1].y);
+  console.log(position[2].x, position[2].y);
+  console.log(direction);
+  for (let positionIndex in position) {
+    let coordinate = position[positionIndex];
+    currentMap[coordinate.y][coordinate.x] = 'X';
+  }
+  food(counter, currentMap);
+  let modifiedMap = table(currentMap);
+  console.log(modifiedMap);
+
+  // let index = readlineSync.keyIn('', { limit: 'wasdq' });
+  // lépések
+  setTimeout(() => {
+    movement(direction);
+    main();
+  }, 100);
+};
+main();
