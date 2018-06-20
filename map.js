f9821eee8b46d1cb22c6e61fd68f72b03165cad2
Gabor Thury <thury.gabor1994@gmail.com>
2018-06-20 15:32:38 +0200

    fixed map

---
+++ map.js
@@ -0,0 +1,48 @@
+const { table } = require('table');
+let output;
+
+let size = 12;
+let map = [];
+
+const mapSnakeGenerate = () => {
+
+  for (let i = 0; i < size; i++) {
+    map[i] = [];
+    for (let j = 0; j < size; j++) {
+      map[i][j] = ' ';
+    }
+  }
+  return map;
+}
+
+let mapSnake = mapSnakeGenerate();
+
+let config = {
+  border: {
+    topBody: `━`,
+    topJoin: `━`,
+    topLeft: `┏`,
+    topRight: `┓`,
+
+    bottomBody: `━`,
+    bottomJoin: `━`,
+    bottomLeft: `┗`,
+    bottomRight: `┛`,
+
+    bodyLeft: `┃`,
+    bodyRight: `┃`,
+    bodyJoin: ` `,
+
+    joinBody: ` `,
+    joinLeft: `┃`,
+    joinRight: `┃`,
+    joinJoin: ` `
+  }
+};
+
+output = table(mapSnake, config);
+console.log(output);
+module.exports = {
+  mapSnake: mapSnake,
+  config: config
+};
