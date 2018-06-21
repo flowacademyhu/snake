cc7956f936391d000770878ce5a89fe3c489f037
Gabor Thury <thury.gabor1994@gmail.com>
2018-06-21 09:37:33 +0200

    hotfix

--- growing.js
+++ growing.js
@@ -2,6 +2,7 @@ const growing = (snake) => {
   let n = snake.length;
   let i = snake[n - 1].x;
   let j = snake[n - 1].y;
-  snake.push({ x: i, y: j });
+  let k = snake[n - 1].char;
+  snake.push({ x: i, y: j, char: k });
 };
 module.exports = growing;
